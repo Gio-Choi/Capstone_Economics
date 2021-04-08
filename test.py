25e1d8b6a1b63b0e45d61aaac223fa504014e8e0
Jamalun <jwtiger22@naver.com>
2021-04-08 19:52:20 +0900

    04.08

---
+++ test.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+list = ['005930','035420','001040']
+
+   for i in list :
+       
+
+b= '003333'
+
+x = [ print b for b in range(len(list))]
+
+# +
+a = []    # 빈 리스트 생성
+ 
+for i in range(3):
+    line = []              # 안쪽 리스트로 사용할 빈 리스트 생성
+    for j in range(2):
+        line.append(0)     # 안쪽 리스트에 0 추가
+    a.append(line)         # 전체 리스트에 안쪽 리스트를 추가
+ 
+print(a)
+# -
+
+x
+
+df = pd.DataFrame(td2,columns = col,index = date)
